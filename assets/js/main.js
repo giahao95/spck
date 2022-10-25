d61831caecc20b4b58d082d4b2078ec7909beef0
Gia Hao <pgiahao11@gmail.com>
2022-10-25 20:16:47 +0700

    update carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,7 @@ const countDown = (y, m, d) => {
   }
 
   if (monthCount < 10) monthCount = "0" + monthCount;
+  $monthEle.innerHTML = monthCount;
 
   // Days countdown
   daysCount = daysOfMonth[month] - now.getDate() + d;
@@ -51,23 +52,21 @@ const countDown = (y, m, d) => {
   }
 
   if (daysCount < 10) daysCount = "0" + daysCount;
+  $daysEle.innerHTML = daysCount;
 
   // Hours contdown
   hoursCount = 23 - now.getHours();
   if (hoursCount < 10) hoursCount = "0" + hoursCount;
+  $hoursEle.innerHTML = hoursCount;
 
   // Minutes Countdown
   minutesCount = 59 - now.getMinutes();
   if (minutesCount < 10) minutesCount = "0" + minutesCount;
+  $minutesEle.innerHTML = minutesCount;
 
   // Seconds minutes
   secondsCount = 59 - now.getSeconds();
   if (secondsCount < 10) secondsCount = "0" + secondsCount;
-
-  $monthEle.innerHTML = monthCount;
-  $daysEle.innerHTML = daysCount;
-  $hoursEle.innerHTML = hoursCount;
-  $minutesEle.innerHTML = minutesCount;
   $secondsEle.innerHTML = secondsCount;
 };
 
@@ -75,17 +74,17 @@ setInterval(() => countDown(2023, 1, 11), 1000);
 
 // Carousel
 const $bannerImg = document.querySelectorAll(".banner-img");
-console.log($bannerImg);
 let i = 0;
+const autoSlider = setInterval(nextControl, 8000);
 
-const nextControl = () => {
+function nextControl() {
   $bannerImg[i].classList.remove("active");
   i = (i + 1) % $bannerImg.length;
   $bannerImg[i].classList.add("active");
-};
+}
 
-const preControl = () => {
+function preControl() {
   $bannerImg[i].classList.remove("active");
   i = (i - 1 + $bannerImg.length) % $bannerImg.length;
   $bannerImg[i].classList.add("active");
-};
+}
